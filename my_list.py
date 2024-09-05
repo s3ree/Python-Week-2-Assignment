2ba54cbebb54dbab3fb3bc37d0fae6d83c02634f
s3ree <davecreation24@gmail.com>
2024-09-05 14:43:41 +0300

    Week 2 Assignment - Python

---
+++ my_list.py
@@ -0,0 +1,21 @@
+my_list = []
+print(my_list)
+
+my_list = [10, 20, 30, 40]
+print(my_list)
+
+my_list.insert(1, 15)
+print(my_list)
+
+my_list.extend([50, 60, 70])
+print(my_list)
+
+my_list.pop()
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+index_of_30 = my_list.index(30)
+print("Index Of value 30 is", index_of_30)
+
